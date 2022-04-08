35d0a33dfffcb13292eb0f337a1ff16565afb4ef
Admin <daipham.3213@gmail.com>
2022-04-08 23:11:56 +0700

    update login

--- StudentMng/Persistence/AppDbInitializer.cs
+++ StudentMng/Persistence/AppDbInitializer.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
+using StudentMng.Models;
 
 namespace StudentMng.Persistence
 {
@@ -12,6 +11,19 @@ namespace StudentMng.Persistence
         protected override void Seed(AppDbContext context)
         {
             base.Seed(context);
+            User admin = new User
+            {
+                Username = "admin",
+                FullName = "Administrator"
+            };
+            using (var sha = new SHA512CryptoServiceProvider())
+            {
+                byte[] password = sha.ComputeHash(Encoding.Default.GetBytes("admin"));;
+                admin.Password = Convert.ToBase64String(password);
+            }
+
+            context.Users.Add(admin);
+            context.SaveChanges();
         }
     }
 }
